d43a833d49ad1206106ac8f2a175d91dfc06792c
Ashok Sasitharan <asasitharan28@gmail.com>
2021-12-10 18:16:03 -0500

    added home screen

---
+++ app/src/main/java/com/example/androidfinal/HomeScreen.java
@@ -0,0 +1,27 @@
+package com.example.androidfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void calcPayrollEventHandler(View view) {
+    }
+
+    public void addPartTimeEventHandler(View view) {
+    }
+
+    public void addFullTimeEventHandler(View view) {
+    }
+
+    public void listAllEmpEventHandler(View view) {
+    }
+}
